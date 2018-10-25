d5a831caeeef7a450e2c344bee6ca816edb9f104
Jose <j.estrada@oh.marketing>
2018-10-25 17:09:54 -0500

    icono

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/white-simbol-di.png" alt="">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/white-simbol-di-seo-main.png" alt="">
                             
                         </a>
 					</div>
