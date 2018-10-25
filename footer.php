96f42e79c1ca82e42ac69719cc6a5d04a4f8e547
Jose <j.estrada@oh.marketing>
2018-10-25 16:57:34 -0500

    script en el footer

--- footer.php
+++ footer.php
@@ -22,14 +22,15 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
         <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+		<script>var cb = function() {
+		var l = document.createElement('link'); l.rel = 'stylesheet';
+		l.href = 'PATH_TO_COMBINED_CSS_FILE';
+		var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+		};
+		var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+		webkitRequestAnimationFrame || msRequestAnimationFrame;
+		if (raf) raf(cb);
+		else window.addEventListener('load', cb);</script>
 
 	</body>
 </html>
