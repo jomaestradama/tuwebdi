610ab5ff1a4a554bd01a589ad5ac6f8f91532caa
Jose <j.estrada@oh.marketing>
2018-10-25 19:29:19 -0500

    items props

--- templates/home.php
+++ templates/home.php
@@ -10,43 +10,60 @@
             <form action=""></form></div>         
                    
         </section>
-         <section id="optimizacion-seo-section" class="hero">
-                    <div class="contenedor-section">
+         <section id="optimizacion-seo-section" class="hero" itemscope itemtype="http://schema.org/Product">
+                    <div class="contenedor-section" >
                         <div class="title-seo-section">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/slider/seofocus.svg" alt="">
-                            <h2 class="title text-centered">
+                            <img itemprop="image" src="<?php echo get_template_directory_uri(); ?>/img/slider/seofocus.svg" alt="">
+                            <h2 itemprop="name" class="title text-centered">
                             Optimización SEO 
                             </h2>
                         </div>
                         <div class="descripcion-seo-section">
                             <p>¿Tus clientes no te encuentran en la web?. Con este servicio te ayudaremos a optimizar tu sitio para que logres posicionarte entre los primeros resultados de búsqueda en Google, Yahoo y Bing. Envíanos la url de tu sitio web y obten <strong>gratis</strong> un análisis SEO completo de tu página.</p>
+                           
                         </div>
                     </div>
+                    <div class="ratings">
+                            <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"></div>
+                            <span itemprop="ratingValue">4.5</span>
+                            de <span itemprop="bestRating">5</span>
+                            basado en <span itemprop="ratingCount">301</span> calificacion de usuarios.
+                    </div>
+                    
                    
+
+
         </section>
 		<!-- section -->
-         <section id="mentory-body">
+         <section id="mentory-body" itemscope itemtype="http://schema.org/Product">
                     <div class="contenedor-section">
                       
                         <div class="descripcion">
                             <p>Creamos páginas web profesionales, creativas y efectivas. Desde carritos de compra tipo ecommerce, CMS's con Wordpress, API´s, Landings Pages para campañas de mailing, <strong>sistemas de reservaciones</strong> para Tours, sistemas de reservaciones para Restaurantes y para <strong>Hoteles</strong></p>
                         </div>
                         <div class="title-section">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/slider/compu.svg" alt="">
-                            <h2 class="title text-centered">
+                            <img itemprop="image" src="<?php echo get_template_directory_uri(); ?>/img/slider/compu.svg" alt="">
+                            <h2 itemprop="name" class="title text-centered">
                             Desarrollo Web Profesional
                             </h2>
                         </div>
                     </div>
+
+                     <div class="ratings">
+                            <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"></div>
+                            <span itemprop="ratingValue">4.5</span>
+                            de <span itemprop="bestRating">5</span>
+                            basado en <span itemprop="ratingCount">301</span> calificacion de usuarios.
+                    </div>
             
         </section>
-        <section id="dis-body">
+        <section id="dis-body" itemscope itemtype="http://schema.org/Product">
                     <div class="contenedor-section">
                       
                         
                         <div class="title-section">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/slider/disenografico.svg" alt="">
-                            <h2 class="title text-centered">
+                            <img itemprop="image" src="<?php echo get_template_directory_uri(); ?>/img/slider/disenografico.svg" alt="">
+                            <h2 itemprop="name" class="title text-centered">
                             Diseño Gráfico
                             </h2>
                         </div>
@@ -56,13 +73,19 @@
                             </p>
                         </div>
                     </div>
+                    <div class="ratings">
+                            <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"></div>
+                            <span itemprop="ratingValue">4.5</span>
+                            de <span itemprop="bestRating">5</span>
+                            basado en <span itemprop="ratingCount">301</span> calificacion de usuarios.
+                    </div>
             
         </section>
-        <section id="responsive-body" class="hero">
+        <section id="responsive-body" class="hero" itemscope itemtype="http://schema.org/Product">
                     <div class="contenedor-section">
                         <div class="title-section">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/slider/movil.svg" alt="">
-                            <h2 class="title text-centered">
+                            <img itemprop="image" src="<?php echo get_template_directory_uri(); ?>/img/slider/movil.svg" alt="">
+                            <h2 itemprop="name" class="title text-centered">
                             Aplicaciones Móviles
                             </h2>
                         </div>
@@ -70,21 +93,33 @@
                             <p>Desarrollo de aplicaciones móviles nativas en Android, IOS y Multiplataforma con soporte para Windows Mobile. Renueva tu negocio y crea una App para optimizar los procesos dentro de tu empresa.</p>
                         </div>
                     </div>
+                    <div class="ratings">
+                            <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"></div>
+                            <span itemprop="ratingValue">4.5</span>
+                            de <span itemprop="bestRating">5</span>
+                            basado en <span itemprop="ratingCount">301</span> calificacion de usuarios.
+                    </div>
                    
         </section>
 
-          <section id="community-body">
+          <section id="community-body" itemscope itemtype="http://schema.org/Product">
                     <div class="contenedor-section">
                       
                         <div class="descripcion">
                             <p>Sabemos que tener presencia en las redes sociales, es escencial para hacer crecer tu negocio. Por suerte contamos con expertos en el tema y podemos ayudarte en la administración de tus redes sociales. Un community manager podrá crearte un perfil en las redes mas populares de la actualidad, como Facebook, Instagram, Twitter y Youtube. Además de podemos ayudarte a gestionar el contenido, tambien podemos crear increibles campañas de publicidad con estrategias <strong>vanguardistas</strong> que dejarán asombrados a todos tus seguidores o clientes.</p>
                         </div>
                         <div class="title-section">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/slider/community-manager-icon.svg" alt="">
-                            <h2 class="title text-centered">
+                            <img itemprop="image" src="<?php echo get_template_directory_uri(); ?>/img/slider/community-manager-icon.svg" alt="">
+                            <h2 itemprop="name" class="title text-centered">
                             Community Management
                             </h2>
                         </div>
+                    </div>
+                    <div class="ratings">
+                            <div itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating"></div>
+                            <span itemprop="ratingValue">4.5</span>
+                            de <span itemprop="bestRating">5</span>
+                            basado en <span itemprop="ratingCount">301</span> calificacion de usuarios.
                     </div>
             
         </section>
